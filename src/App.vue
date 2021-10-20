71a60432616064959da0d7b503dc0d0dd295f4ca
Tyler Petz <tylerp@booj.com>
2021-10-20 14:22:26 -0600

    gradients

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, reactive } from 'vue'
 
+const gradientDirections = ['to top', 'to left', 'to right', 'to bottom']
 const fields = {
   buildName: 'Build Name',
   keyboardName: 'Keyboard Name',
@@ -9,12 +10,12 @@ const fields = {
   stabs: 'Stabilizers',
   moreText: 'More Text'
 }
-
 const card = ref({ ...fields })
-const style = ref({
-  bgDirection: 'br',
-  bgColor1: '#555555',
-  bgColor2: '#000000',
+
+
+const style = reactive({
+  bgDirection: 'to left',
+  bgColors: ['#555555'],
   textColor: '#ffffff',
   textOrientation: 'left'
 })
@@ -22,49 +23,73 @@ const style = ref({
 function print() {
   window.print()
 }
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+function addStyle() {
+  style.bgColors.push(style.bgColors.at(-1))
+}
+
+function removeStyle() {
+  style.bgColors.splice(-1)
+}
+
+const cardStyles = computed(() => ({
+  background: style.bgColors.length > 1 ? `linear-gradient(${style.bgDirection},${style.bgColors.join(',')})` : style.bgColors[0],
+  color: style.textColor,
+  'text-align': style.textOrientation,
+}))
 </script>
 
 <template>
-  <main class="flex flex-row h-screen bg-white">
+  <main class="flex flex-row h-screen bg-white relative text-gray-700">
     <form class="flex flex-col w-1/4 print:hidden p-3 space-y-4">
       <h1 class="text-center text-xl font-bold text-gray-900 py-4">Keyboard ID Card Printer</h1>
       <div v-for="field in Object.entries(fields)" :key="field[0]">
         <label class="block text-sm font-medium text-gray-700">
           {{ field[1] }}
-          <input type="text" placeholder="" v-model="card[field[0]]" class="mt-1 appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+          <input type="text" placeholder="" v-model="card[field[0]]" class="mt-1 appearance-none block w-full px-3 py-2 border  rounded-md shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
         </label>
       </div>
-      <div class="flex flex-row justify-evenly">
-        <label class="flex flex-col items-center text-sm font-medium text-gray-700">
+      <div class="grid grid-cols-2 gap-4">
+        <label class="flex flex-col items-center text-sm font-medium">
           Background
-          <input type="color" v-model="style.bgColor1" class="block appearance-none shadow-sm h-10">
+          <div class="flex items-center flex-row w-full space-x-2">
+            <input tabindex="-1" v-for="(color, index) in style.bgColors" :index="color" type="color" v-model="style.bgColors[index]" class="bg-transparent appearance-none shadow-sm h-10 w-full" />
+            <button v-if="style.bgColors.length > 1 && style.bgColors.length < 4" @click.native="removeStyle" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+            <button v-if="style.bgColors.length < 3" @click.native="addStyle" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </button>
+          </div>
+        </label>
+        <label class="flex flex-col items-center text-sm font-medium">
+          Gradient Direction
+          <select v-model="style.bgDirection" class="w-full max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+            <option v-for="direction in gradientDirections" :key="direction">{{ direction }}</option>
+          </select>
         </label>
         <label class="flex flex-col items-center text-sm font-medium text-gray-700">
           Text Color
           <input type="color" v-model="style.textColor" class="block appearance-none shadow-sm h-10">
         </label>
-         <label class="flex flex-col items-center text-sm font-medium text-gray-700">
+        <label class="flex flex-col items-center text-sm font-medium">
           Text Orientation
-          <select v-model="style.textOrientation" class="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+          <select v-model="style.textOrientation" class="w-full max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
             <option value="left">left</option>
             <option value="center">center</option>
             <option value="right">right</option>
           </select>
         </label>
       </div>
-      <!-- <div>
-        <label class="block text-sm font-medium text-gray-700">
-          Background 2
-          <input type="color" v-model="style.bgColor2" class="block appearance-none shadow-sm">
-        </label>
-      </div> -->
     </form>
     <section class="w-3/4 bg-gray-200 flex flex-col items-center justify-center print:bg-white print:items-start print:justify-start">
       <div 
         class="card flex flex-col space-y-2 justify-center w-[502px] h-[325px] p-12 shadow-lg relative print:shadow-none print:p-16"
-        :style="`background-color: ${style.bgColor1}; color: ${style.textColor}; text-align: ${style.textOrientation}`"
+        :style="cardStyles"
       >
         <div class="border border-white absolute m-4 inset-0 border-dashed hidden print:block" />
         <div v-if="card.buildName" class="text-3xl pb-4 font-bold">{{ card.buildName }}</div>
@@ -76,5 +101,8 @@ function print() {
       </div>
       <button class="mt-4 print:hidden" @click="print">Print</button>
     </section>
+    <footer class="absolute right-0 bottom-0 text-xs p-4 flex flex-row items-center space-x-4">
+      <a href="https://github.com/tylerpetz/keyboard-id-card-printer/issues" target="_blank">Issues</a>
+    </footer>
   </main>
 </template>
